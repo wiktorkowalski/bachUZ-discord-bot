6c5431b85a87537b2c18002b453c856a2cf52cbc
Mikołaj Kalwa <13047857+mikolajkalwa@users.noreply.github.com>
2020-04-02 23:13:32 +0200

    Changes (#1)
    Improved ping command
    Bot wont respond to uknown commands
    Configurable prefix

--- BachUZ/Modules/InfoModule.cs
+++ BachUZ/Modules/InfoModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Discord.Commands;
 
 namespace BachUZ.Modules
@@ -7,6 +8,12 @@ namespace BachUZ.Modules
     {
         [Command("ping")]
         [Summary("Checks if bot is alive or not.")]
-        public Task Ping() => ReplyAsync("pong!");
+        public async Task Ping()
+        {
+            var sw = Stopwatch.StartNew();
+            var msg = await Context.Channel.SendMessageAsync("Pong!").ConfigureAwait(false);
+            sw.Stop();
+            await msg.ModifyAsync(m => m.Content = $"Pong! ({(int)sw.Elapsed.TotalMilliseconds}ms)").ConfigureAwait(false);
+        }
     }
 }
