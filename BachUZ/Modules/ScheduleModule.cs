1fef9ca5aea406c368db6e4ec25c40d194bc0b18
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-04-10 17:22:06 +0200

    Class schedule command

---
+++ BachUZ/Modules/ScheduleModule.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.Commands;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+using System;
+using BachUZ.Services;
+
+namespace BachUZ.Modules
+{
+    public class ScheduleModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("schedule")]
+        [Alias("plan")]
+        [Summary("Looks for a specified group class schedule")]
+        public async Task Schedule(string groupName)
+        {
+            var matchingKeys = UZScheduleService.ScheduleCache.Keys.Where(x => x.Contains(groupName.ToLower()));
+            if (matchingKeys.Count() > 1)
+            {
+                var matchingGroups = string.Join(", ", matchingKeys.ToArray());
+                //var url = PlanService.ScheduleCache[groupName];
+                await Context.Channel.SendMessageAsync($"Specify the group: {matchingGroups}.");
+            }
+            else if (!matchingKeys.Any())
+            {
+                await Context.Channel.SendMessageAsync("Class schedule not found.");
+            }
+            else
+            {
+                var matchingKey = matchingKeys.ToArray()[0];
+                await Context.Channel.SendMessageAsync(
+                    $"Class schedule: http://plan.uz.zgora.pl/{UZScheduleService.ScheduleCache[matchingKey]}");
+            }
+        }
+    }
+}
--- BachUZ/Program.cs
+++ BachUZ/Program.cs
@@ -18,14 +18,14 @@ namespace BachUZ
 
         public async Task MainAsync()
         {
+            await UZScheduleService.PopulateScheduleCache();
             _config = BuildConfig();
 
             using var services = ConfigureServices();
             var client = services.GetRequiredService<DiscordSocketClient>();
-
+                
             client.Log += LogAsync;
             services.GetRequiredService<CommandService>().Log += LogAsync;
-
             await client.LoginAsync(TokenType.Bot, _config["token"]);
             await client.StartAsync();
 
