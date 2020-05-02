348e942b7a0c9bce001a4463febf70172773690a
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-05-03 00:24:32 +0200

    Add generated database models and context

---
+++ BachUZ.Database/Guilds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BachUZ.Database
+{
+    public partial class Guilds
+    {
+        public Guilds()
+        {
+            Emotes = new HashSet<Emotes>();
+            UsersGuilds = new HashSet<UsersGuilds>();
+            UsersOnVoice = new HashSet<UsersOnVoice>();
+        }
+
+        public decimal GuildId { get; set; }
+        public string CustomPrefix { get; set; }
+
+        public virtual ICollection<Emotes> Emotes { get; set; }
+        public virtual ICollection<UsersGuilds> UsersGuilds { get; set; }
+        public virtual ICollection<UsersOnVoice> UsersOnVoice { get; set; }
+    }
+}
---
+++ BachUZ.Database/UsersOnVoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BachUZ.Database
+{
+    public partial class UsersOnVoice
+    {
+        public decimal UserId { get; set; }
+        public decimal VoiceChannelId { get; set; }
+        public decimal GuildId { get; set; }
+        public int Time { get; set; }
+
+        public virtual Guilds Guild { get; set; }
+        public virtual Users User { get; set; }
+    }
+}
