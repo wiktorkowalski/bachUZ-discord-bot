6b70a944f00a62ecaec632e8aa44109339c0069e
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-05-06 17:59:08 +0200

    Port BachUZ to ASP.NET (#14)

---
+++ BachUZ.Discord/Events/JoinedGuild.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BachUZ.Database;
+using Discord.WebSocket;
+
+namespace BachUZ.Events
+{
+    static class JoinedGuild
+    {
+        internal static async Task HandleEvent(SocketGuild guild)
+        {
+            await using (var database = new BachuzContext())
+            {
+                Guilds newGuild = new Guilds
+                {
+                    GuildId = guild.Id
+                };
+                await database.Guilds.AddAsync(newGuild);
+
+                var emotes = new List<Emotes>();
+
+                foreach (var guildEmote in guild.Emotes)
+                {
+                    emotes.Add(new Emotes
+                    {
+                        EmoteId = guildEmote.Id,
+                        Name = guildEmote.Name,
+                        Count = 0,
+                        GuildId = guild.Id,
+                    });
+                }
+
+                await database.Emotes.AddRangeAsync(emotes);
+                await database.SaveChangesAsync();
+            }
+        }
+    }
+}
