0b72d0b8eaf73094069cfc5d85fe2b2480c1cace
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-06-01 16:54:11 +0200

    website (#19)

---
+++ BachUZ.Website/Data/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BachUZ.Website.Data
+{
+    public class UserInfo
+    {
+        public ulong Id { get; set; }
+        public string Username { get; set; }
+        public string Discriminator { get; set; }
+        public string Avatar { get; set; }
+        public bool Verified { get; set; }
+        public string Email { get; set; }
+        public int Flags { get; set; }
+        public int Premium_type { get; set; }
+        public int Public_flags { get; set; }
+
+    }
+}
